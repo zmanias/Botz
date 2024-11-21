39cf9c97383c2c637997ab31cc3d8b49a6872460
CyberIndo <55630381+zmanias@users.noreply.github.com>
2024-11-21 07:04:28 +0700

    Add files via upload

---
+++ all/database/kosong.js
@@ -0,0 +1,1 @@
+"Buy Script Bot ? Chat 6285624297893"
---
+++ all/database/teksjpm.js
@@ -0,0 +1,36 @@
+*All Transaksi Open ✅*
+
+*List Market - 🛒*
+* Panel Run Bot WhatsApp
+* Join Own/Seller Bot Subdomain
+* Domain/Subdomain (my.id/biz.id dll)
+* Nokos WhatsApp (+62)
+* Jasa Unban WhatsApp
+* Jasa Install Panel/Tema Panel
+* Jasa Tambah/Edit Fitur SC Bot
+* Jasa Fix Error SC Bot
+* Jasa Ubah SC Bot Scan To Pairing Code
+* Jasa Suntik All Sosmed
+* SC Tema Panel Enigma/Stellar
+* SC Bot Campuran (Button)
+* SC JagaGrup (antilinkgc)
+* SC Subdomain 60 Domain
+* DLL Tanyakan Saja
+
+*List Harga Panel Pterodactyl 🚀*
+* Ram 1GB : Rp1000
+* Ram 2GB : Rp3000
+* Ram 3GB : Rp5000
+* Ram 4GB : Rp7000
+* Ram Unlimited : Rp10.000
+*Bergaransi & Server Private*
+
+*Minat ? Hubungi :*
+* 🪀 WhatsApp
+https://wa.me/-
+* 🚀 Telegram
+https://t.me/-
+* 🌀 Testimoni
+https://whatsapp.com/channel/-
+
+https://chat.whatsapp.com/-
